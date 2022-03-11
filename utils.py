3fa9d686dc35335f84916938341fa9060eeffc70
Vasilii Kovalchuk <sovamorco@sovamor.co>
2022-03-11 20:09:55 +0200

    end of life

--- utils.py
+++ utils.py
@@ -94,17 +94,17 @@ class Settings:
 
     def __init__(self, settings):
         self.APP_NAME = 'BHBot'
-        self.APP_VERSION = '3.5.1-beta'
+        self.APP_VERSION = '3.5.1'
         self.APP_CHANGELOGS = {
             'English': [
                 f'Updated to {self.APP_VERSION} \\o/',
                 'If it\'s your first time using the bot or seeing this message, please click "Instructions" and read them carefully',
-                '- Changed the mode',
+                '- Bot is no longer maintained',
             ],
             'Русский': [
                 f'Обновился до {self.APP_VERSION} \\o/',
                 'Если вы используете бота или видите это сообщение впервые, пожалуйста, нажмите на "Инструкции" и тщательно их прочтите',
-                '- Сменил режим игры',
+                '- Бот больше не поддерживается',
             ]
         }
 
