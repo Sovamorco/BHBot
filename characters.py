a8be178ab2c8ca6379b1a7b679bc373a12387080
Vasilii Kovalchuk <sovamorco@sovamor.co>
2023-03-31 22:59:27 +0400

    add new character

--- characters.py
+++ characters.py
@@ -2,7 +2,8 @@ from levels import *
 
 characters = ['bödvar', 'cassidy', 'orion', 'lord vraxx', 'gnash', 'queen nai', 'hattori', 'sir roland', 'scarlet', 'thatch', 'ada', 'sentinel', 'lucien', 'teros', 'brynn', 'asuri', 'barraza',
               'ember', 'azoth', 'koji', 'ulgrim', 'diana', 'jhala', 'kor', 'wu shang', 'val', 'ragnir', 'cross', 'mirage', 'nix', 'mordex', 'yumiko', 'artemis', 'caspian', 'sidra', 'xull', 'kaya',
-              'isaiah', 'jiro', 'lin fei', 'zariel', 'rayman', 'dusk', 'fait', 'thor', 'petra', 'vector', 'volkov', 'onyx', 'jaeyun', 'mako', 'magyar', 'reno', 'munin', 'arcadia', 'ezio', 'tezca']
+              'isaiah', 'jiro', 'lin fei', 'zariel', 'rayman', 'dusk', 'fait', 'thor', 'petra', 'vector', 'volkov', 'onyx', 'jaeyun', 'mako', 'magyar', 'reno', 'munin', 'arcadia', 'ezio', 'tezca',
+              'thea']
 
 level_character_matrix_width = 15
 level_character_matrix = list([characters[i:i + level_character_matrix_width] for i in range(0, len(characters), level_character_matrix_width)])
--- utils.py
+++ utils.py
@@ -94,19 +94,17 @@ class Settings:
 
     def __init__(self, settings):
         self.APP_NAME = 'BHBot'
-        self.APP_VERSION = '3.5.18'
+        self.APP_VERSION = '3.5.19'
         self.APP_CHANGELOGS = {
             'English': [
                 f'Updated to {self.APP_VERSION} \\o/',
                 'If it\'s your first time using the bot or seeing this message, please click "Instructions" and read them carefully',
-                '- Fixed unlocked character detection',
-                '- Fixed bot autostarting during update',
+                '- Added Thea',
             ],
             'Русский': [
                 f'Обновился до {self.APP_VERSION} \\o/',
                 'Если вы используете бота или видите это сообщение впервые, пожалуйста, нажмите на "Инструкции" и тщательно их прочтите',
-                '- Починил определение доступных персонажей',
-                '- Починил автозапуск во время обновления',
+                '- Добавил нового персонажа',
             ]
         }
 
